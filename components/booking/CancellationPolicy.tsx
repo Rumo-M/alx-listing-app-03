d51bf6c312d662891d270b1fe1aa68e42a444502
Martha Maloba <marthamarumo92@gmail.com>
2025-08-15 23:27:03 +0200

    Create CancellationPolicy

---
+++ components/booking/CancellationPolicy.tsx
@@ -0,0 +1,14 @@
+// components/booking/CancellationPolicy.tsx
+
+const CancellationPolicy: React.FC = () => {
+  return (
+    <div className="cancellation-policy">
+      <h2>Cancellation Policy</h2>
+      <p>
+        Please be advised that cancellations made within 24 hours of the scheduled date may incur a penalty. If you need to cancel or reschedule, please contact us at least 48 hours in advance.
+      </p>
+    </div>
+  );
+};
+
+export default CancellationPolicy;
